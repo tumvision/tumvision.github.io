24ad09061a14bb5654b02c1485ad0014654e244f
robinborth <robin.borth@tum.de>
2024-09-12 22:25:54 +0200

    fix: github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
     output: "export",
-    reactStrictMode: true,
-    images: {unoptimized: true},
-};
-
-export default nextConfig;
+  
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
