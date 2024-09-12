5fc500ce83f933cbf2e64b93fb3871168606812c
robinborth <robin.borth@tum.de>
2024-09-12 22:47:15 +0200

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "TUMVision",
   description: "3D Computer Vision Club",
+  icons: {
+    icon: "/favicon.ico", // Path to your favicon
+  },
 };
 
 export default function RootLayout({
