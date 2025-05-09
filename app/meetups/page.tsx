1bb8227452ed27d523e3682026d4db8e07f5fd6a
Robin Borth <58107174+robinborth@users.noreply.github.com>
2025-05-09 11:27:15 +0200

    Update page.tsx

--- app/meetups/page.tsx
+++ app/meetups/page.tsx
@@ -14,10 +14,25 @@ export default function Meetups() {
       </Text>
       <Headline className="text-xl mt-4">Upcoming Events</Headline>
       <ReadingGroup
-        speaker="Shivangi Aneja"
-        time="05.12.2024 18:00-19:00"
-        title="GaussianSpeech: Audio-Driven Gaussian Avatars"
-        paper="https://shivangi-aneja.github.io/projects/gaussianspeech"
+        speaker="Mateo de Mayo"
+        time="22.05.2025 18:00-19:00"
+        title="Visual-inertial tracking for Spatial Agents"
+        paper="https://cvg.cit.tum.de/members/mayom"
+        className="mt-2"
+      />
+      <Headline className="text-xl mt-4">Previous Events</Headline>
+      <ReadingGroup
+        speaker="Artem Sevastopolsky"
+        time="06.02.2025 18:00-19:00"
+        title="One-Shot Head Reconstruction Model with Virtual Views"
+        paper="https://seva100.github.io/"
+        className="mt-2"
+      />
+      <ReadingGroup
+        speaker="Simon Weber"
+        time="30.01.2025 18:00-19:00"
+        title="Power Variable Projection for Initialization-Free Large-Scale Bundle Adjustment"
+        paper="https://lnkd.in/dn9gcHay"
         className="mt-2"
       />
       <ReadingGroup
@@ -27,26 +42,18 @@ export default function Meetups() {
         paper="https://daoyig.github.io/"
         className="mt-2"
       />
-      <Headline className="text-xl mt-4">Previous Events</Headline>
       <ReadingGroup
-        speaker="Peter Kocsis"
-        time="17.10.2024 18:00-19:00"
-        title="Intrinsic Image Diffusion for Indoor Single-view Material Estimation"
-        paper="https://peter-kocsis.github.io/IntrinsicImageDiffusion/"
+        speaker="Shivangi Aneja"
+        time="05.12.2024 18:00-19:00"
+        title="GaussianSpeech: Audio-Driven Gaussian Avatars"
+        paper="https://shivangi-aneja.github.io/projects/gaussianspeech"
         className="mt-2"
       />
       <ReadingGroup
-        speaker="Lukas Höllein"
-        time="24.10.2024 18:00-19:00"
-        title="Text2Room: Extracting Textured 3D Meshes from 2D Text-to-Image Models"
-        paper="https://lukashoel.github.io/text-to-room/"
-        className="mt-2"
-      />
-      <ReadingGroup
-        speaker="Simon Giebenhain"
-        time="31.10.2024 18:00-19:00"
-        title="NPHM: Learning Neural Parametric Head Models"
-        paper="https://simongiebenhain.github.io/NPHM/"
+        speaker="Yujin Chen"
+        time="28.11.2024 18:00-19:00"
+        title="Mesh2NeRF: Direct Mesh Supervision for Neural Radiance Field Representation and Generation"
+        paper="https://terencecyj.github.io/projects/Mesh2NeRF/"
         className="mt-2"
       />
       <ReadingGroup
@@ -57,12 +64,27 @@ export default function Meetups() {
         className="mt-2"
       />
       <ReadingGroup
-        speaker="Yujin Chen"
-        time="28.11.2024 18:00-19:00"
-        title="Mesh2NeRF: Direct Mesh Supervision for Neural Radiance Field Representation and Generation"
-        paper="https://terencecyj.github.io/projects/Mesh2NeRF/"
+        speaker="Simon Giebenhain"
+        time="31.10.2024 18:00-19:00"
+        title="NPHM: Learning Neural Parametric Head Models"
+        paper="https://simongiebenhain.github.io/NPHM/"
         className="mt-2"
       />
+      <ReadingGroup
+        speaker="Lukas Höllein"
+        time="24.10.2024 18:00-19:00"
+        title="Text2Room: Extracting Textured 3D Meshes from 2D Text-to-Image Models"
+        paper="https://lukashoel.github.io/text-to-room/"
+        className="mt-2"
+      />
+      <ReadingGroup
+        speaker="Peter Kocsis"
+        time="17.10.2024 18:00-19:00"
+        title="Intrinsic Image Diffusion for Indoor Single-view Material Estimation"
+        paper="https://peter-kocsis.github.io/IntrinsicImageDiffusion/"
+        className="mt-2"
+      />
+
     </Container>
   );
 }
