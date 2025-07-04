91fdd378918994c70aa34e35c8e30f00489dad2b
Robin Borth <58107174+robinborth@users.noreply.github.com>
2025-07-04 15:31:51 +0200

    Update page.tsx

--- app/meetups/page.tsx
+++ app/meetups/page.tsx
@@ -14,20 +14,6 @@ export default function Meetups() {
       </Text>
       <Headline className="text-xl mt-4">Upcoming Events</Headline>
       <ReadingGroup
-        speaker="Xinyi Zhang"
-        time="26.06.2025 18:00-19:00"
-        title="DNF: Unconditional 4D Generation with Dictionary-based Neural Fields"
-        paper="https://xzhang-t.github.io/project/DNF/"
-        className="mt-2"
-      />
-      <ReadingGroup
-        speaker="Ziya Erkoc"
-        time="03.07.2025 18:00-19:00"
-        title="PrEditor3D: Fast and Precise 3D Shape Editing"
-        paper="https://ziyaerkoc.com/preditor3d/"
-        className="mt-2"
-      />
-      <ReadingGroup
         speaker="Jiapeng Tang"
         time="10.07.2025 18:00-19:00"
         title="GAF: Gaussian Avatar Reconstruction from Monocular Videos via Multi-view Diffusion"
@@ -49,6 +35,20 @@ export default function Meetups() {
         className="mt-2"
       />
       <Headline className="text-xl mt-4">Previous Events</Headline>
+      <ReadingGroup
+        speaker="Ziya Erkoc"
+        time="03.07.2025 18:00-19:00"
+        title="PrEditor3D: Fast and Precise 3D Shape Editing"
+        paper="https://ziyaerkoc.com/preditor3d/"
+        className="mt-2"
+      />
+      <ReadingGroup
+        speaker="Xinyi Zhang"
+        time="26.06.2025 18:00-19:00"
+        title="DNF: Unconditional 4D Generation with Dictionary-based Neural Fields"
+        paper="https://xzhang-t.github.io/project/DNF/"
+        className="mt-2"
+      />
       <ReadingGroup
         speaker="Haoxuan Li"
         time="12.06.2025 18:00-19:00"
